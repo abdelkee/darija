e1ef8a5ed3e8d1c1445d45150e324870348992a5
abdelkee <abdell.anejjar@gmail.com>
2022-12-28 17:24:50 -0500

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title></title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/logo.png"></link>
+      <meta name="theme-color" content="#dc2626" />
+    </>
+  );
+}
